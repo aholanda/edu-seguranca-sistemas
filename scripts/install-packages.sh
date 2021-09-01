15a99fcb45e7176e7631ba5733d10e2765cf3ec5
Adriano J. Holanda <aholanda@usp.br>
2021-09-01 09:57:39 -0300

    1st commit

---
+++ scripts/install-packages.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Script para instalar programas no SO Linux
+# necessários para as demonstrações e exercícios.
+
+case $1 in
+    net)        
+        sudo apt update && \
+            sudo apt install \
+                hping \
+                tcpdump \
+                wireshark
+        ;;
+    *)
+        echo "usage: $0 [net]"
+        exit -1
+esac
+
+exit 0
