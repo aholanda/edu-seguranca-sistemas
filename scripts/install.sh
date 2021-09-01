8bc1431c7930037ad5b15562315aae4723724e47
Adriano J. Holanda <aholanda@usp.br>
2021-09-01 16:16:56 -0300

    include dev packages

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,29 +1,43 @@
 #!/usr/bin/env bash
 
+##
+# Autor: Adriano J. Holanda
+# Script para instalar programas no SO Linux
+# necessários para as demonstrações e exercícios.
+##
+
+INSTALL="sudo apt install -y"
+UPDATE="sudo apt update"
+UPGRADE="sudo apt upgrade -y"
+
 function install_core_packages {
     pkgs="clang gcc tcc vim"
     for pkg in pkgs; do 
-        $(test $pkg) && sudo apt install -y $pkg
+        $(test $pkg) && ${INSTALL} -y $pkg
     done
 }
 
 function upgrade {
-    sudo apt update && sudo apt upgrade -y
+    ${UPGRADE}
 }
 
-# Script para instalar programas no SO Linux
-# necessários para as demonstrações e exercícios.
-
 case $1 in
+    dev)
+        ${UPDATE} && \
+            ${INSTALL} \
+                clang gcc tcc \
+                apache2 php \
+                postgresql php-pgsql
     net)        
-        sudo apt update && \
-            sudo apt install \
+        ${UPDATE} && \
+            ${INSTALL} \
                 hping3 \
-                tcpdump \
-                wireshark
+                md5sum \
+                nmap \
+                tcpdump 
         ;;
     *)
-        echo "usage: $0 [net]"
+        echo "usage: $0 [dev|net]"
         exit -1
 esac
 
